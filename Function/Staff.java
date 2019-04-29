99b7d950e511e0b3a08ca2c0b40328886ebccd51
jose <47397412+jose806@users.noreply.github.com>
2019-04-29 18:13:54 -0500

    Created Staff/behavior

---
+++ Function/Staff.java
@@ -0,0 +1,33 @@
+package Function;
+
+public class Staff {
+    String fName;
+    String lName;
+    int id;
+
+    public Staff(String a, String b, int c)
+    {
+        fName = a;
+        lName = b;
+        id = c;
+    }
+
+    public String getfName()
+    {
+        return fName;
+
+    }
+
+    public String getlName()
+    {
+        return lName;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+
+}
+
