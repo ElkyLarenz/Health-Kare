5d2c1e3c0ffe75418856097cc3e5b83627b596d6
ChMoua14 <tclmedu@gmail.com>
2019-04-30 19:29:58 -0500

    Update AdminLogin.java

--- GUI/AdminLogin.java
+++ GUI/AdminLogin.java
@@ -1,11 +1,5 @@
 package GUI;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import javax.swing.JOptionPane;
-
 public class AdminLogin extends javax.swing.JFrame {
 
     /**
@@ -16,7 +10,7 @@ public class AdminLogin extends javax.swing.JFrame {
         this.setVisible(true);
         this.setResizable(false);
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -24,7 +18,7 @@ public class AdminLogin extends javax.swing.JFrame {
         titlePnl = new javax.swing.JPanel();
         titleLbl = new javax.swing.JLabel();
         nameTxtFld = new javax.swing.JTextField();
-        passTxtFld = new javax.swing.JPasswordField();
+        passTxtFld = new javax.swing.JTextField();
         loginBtn = new javax.swing.JButton();
         backBtn = new javax.swing.JButton();
 
@@ -40,18 +34,18 @@ public class AdminLogin extends javax.swing.JFrame {
         javax.swing.GroupLayout titlePnlLayout = new javax.swing.GroupLayout(titlePnl);
         titlePnl.setLayout(titlePnlLayout);
         titlePnlLayout.setHorizontalGroup(
-                titlePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(titlePnlLayout.createSequentialGroup()
-                                .addGap(48, 48, 48)
-                                .addComponent(titleLbl)
-                                .addContainerGap(53, Short.MAX_VALUE))
+            titlePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(titlePnlLayout.createSequentialGroup()
+                .addGap(48, 48, 48)
+                .addComponent(titleLbl)
+                .addContainerGap(53, Short.MAX_VALUE))
         );
         titlePnlLayout.setVerticalGroup(
-                titlePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(titlePnlLayout.createSequentialGroup()
-                                .addGap(133, 133, 133)
-                                .addComponent(titleLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            titlePnlLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(titlePnlLayout.createSequentialGroup()
+                .addGap(133, 133, 133)
+                .addComponent(titleLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         nameTxtFld.setText("Name");
@@ -85,32 +79,32 @@ public class AdminLogin extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                                .addComponent(titlePnl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                        .addGroup(layout.createSequentialGroup()
-                                                .addComponent(backBtn)
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                .addComponent(loginBtn))
-                                        .addComponent(nameTxtFld, javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(passTxtFld, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(21, 21, 21))
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(titlePnl, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(backBtn)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(loginBtn))
+                    .addComponent(nameTxtFld, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(passTxtFld, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(21, 21, 21))
         );
         layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(titlePnl, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addContainerGap(120, Short.MAX_VALUE)
-                                .addComponent(nameTxtFld, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(passTxtFld, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(loginBtn)
-                                        .addComponent(backBtn))
-                                .addGap(116, 116, 116))
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(titlePnl, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(120, Short.MAX_VALUE)
+                .addComponent(nameTxtFld, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(passTxtFld, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(loginBtn)
+                    .addComponent(backBtn))
+                .addGap(116, 116, 116))
         );
 
         pack();
@@ -118,41 +112,14 @@ public class AdminLogin extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void nameTxtFldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameTxtFldActionPerformed
-
+        
     }//GEN-LAST:event_nameTxtFldActionPerformed
 
     private void passTxtFldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passTxtFldActionPerformed
-
+        
     }//GEN-LAST:event_passTxtFldActionPerformed
 
     private void loginBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBtnActionPerformed
-        String sql = "select * from Admin where username = ? and passwrd = ?";
-
-        try{
-
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "Class410");
-            System.out.println("Database connected!");
-            PreparedStatement stmt = con.prepareStatement(sql);
-            stmt.setString(1,nameTxtFld.getText());
-            stmt.setString(2,new String(passTxtFld.getPassword()));
-
-            ResultSet rs = stmt.executeQuery();
-            if(rs.next())
-            {
-                JOptionPane.showMessageDialog(null, "Welcome " + nameTxtFld.getText(), "Login Successful", JOptionPane.PLAIN_MESSAGE);
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(null, "Invalid username/password ", "Login unsuccessful", JOptionPane.ERROR_MESSAGE);
-            }
-
-            con.close();
-        }
-        catch(Exception e)
-        {
-            JOptionPane.showMessageDialog(null, e);
-        }
         AdminMenu.main(new String[] {});
         this.dispose();
     }//GEN-LAST:event_loginBtnActionPerformed
@@ -163,12 +130,12 @@ public class AdminLogin extends javax.swing.JFrame {
     }//GEN-LAST:event_backBtnActionPerformed
 
     public static void main(String args[]) {
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new AdminLogin();
-
+                
             }
         });
     }
@@ -177,7 +144,7 @@ public class AdminLogin extends javax.swing.JFrame {
     private javax.swing.JButton backBtn;
     private javax.swing.JButton loginBtn;
     private javax.swing.JTextField nameTxtFld;
-    private javax.swing.JPasswordField passTxtFld;
+    private javax.swing.JTextField passTxtFld;
     private javax.swing.JLabel titleLbl;
     private javax.swing.JPanel titlePnl;
     // End of variables declaration//GEN-END:variables
